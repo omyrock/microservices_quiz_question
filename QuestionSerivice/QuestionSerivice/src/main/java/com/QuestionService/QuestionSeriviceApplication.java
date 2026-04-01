b7772b8803bcdc5a9da5532ab6b27cb6b8a6cddd
Omvir Singh <omvir8419@gmail.com>
2026-04-02 01:00:36 +0530

    Initial commit with 4 Spring Boot microservices

---
+++ QuestionSerivice/QuestionSerivice/src/main/java/com/QuestionService/QuestionSeriviceApplication.java
@@ -0,0 +1,13 @@
+package com.QuestionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionSeriviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionSeriviceApplication.class, args);
+	}
+
+}
